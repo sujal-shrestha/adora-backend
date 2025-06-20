93e87eed078894eed3222bd76f0398d5e04e217a
sujal-shrestha <sujalwork69@gmail.com>
2025-06-21 00:18:13 +0545

    chore: update CORS settings and add userRoutes

--- server.js
+++ server.js
@@ -4,12 +4,16 @@ import cors from 'cors';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
 import campaignRoutes from './routes/campaignRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+
 dotenv.config();
 
 const app = express();
 
 app.use(cors({
   origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 app.use(express.json());
@@ -17,6 +21,8 @@ app.use(express.json());
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/campaigns', campaignRoutes);
+app.use('/api/users', userRoutes);
+
 
 const PORT = process.env.PORT || 10010;
 
