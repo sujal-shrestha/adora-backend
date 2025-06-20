d6ec327b75e1353e8aa7a2b1fc5ed025de90c7a5
sujal-shrestha <sujalwork69@gmail.com>
2025-06-20 23:25:48 +0545

    fix: auth middleware parse token correctly

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 const auth = (req, res, next) => {
-  const token = req.header('Authorization');
+  const token = req.header('Authorization')?.split(' ')[1];
   if (!token) return res.status(401).json({ message: 'Access denied' });
 
   try {
