e283a250440f2159a290b84e27a534bab5e28468
sujal-shrestha <sujalwork69@gmail.com>
2025-07-29 18:37:56 +0545

    chore: register media routes in Express server

--- server.js
+++ server.js
@@ -1,30 +1,8 @@
-import express from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors';
-import connectDB from './config/db.js';
-import authRoutes from './routes/authRoutes.js';
-import campaignRoutes from './routes/campaignRoutes.js';
-import userRoutes from './routes/userRoutes.js';
 import connectDB from './config/db.js';
 import app from './app.js';
 
 dotenv.config();
-
-const app = express();
-
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
-
-app.use(express.json());
-
-// Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/campaigns', campaignRoutes);
-app.use('/api/users', userRoutes);
-
 
 const PORT = process.env.PORT || 10010;
 
